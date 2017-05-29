9d4bf4347ef6538dbeb10d73774c15866ecc6b62
stupid <sunfeng32@qq.com>
2017-05-28 18:23:26 -0700

    test(hocs/extendStatics): with related hocs

---
+++ src/hocs/__tests__/extendPropTypes.test.js
@@ -0,0 +1,33 @@
+/* eslint-env jest */
+import React from 'react';
+import T from 'prop-types';
+import extendPropTypes from '../extendPropTypes';
+
+test('extendPropTypes extends the propTypes of the base component', () => {
+  const BaseComponent = () => <div />;
+  const propTypes = {
+    foo: T.number.isRequired,
+    bar: T.number.isRequired,
+  };
+  BaseComponent.propTypes = propTypes;
+
+  const NewComponent = extendPropTypes({ foo: T.number })(BaseComponent);
+
+  expect(NewComponent).toBe(BaseComponent);
+  expect(NewComponent.propTypes).not.toBe(propTypes);
+  expect(NewComponent.propTypes).toEqual({
+    foo: T.number,
+    bar: T.number.isRequired,
+  });
+});
+
+test('extendPropTypes does not change the propTypes of the base component if input is falsy', () => {
+  const BaseComponent = () => <div />;
+  const propTypes = {};
+  BaseComponent.propTypes = propTypes;
+
+  const NewComponent = extendPropTypes(undefined)(BaseComponent);
+
+  expect(NewComponent).toBe(BaseComponent);
+  expect(NewComponent.propTypes).toBe(propTypes);
+});
