20caeca2fa8f785639f9144a3182f1ab1841b66f
stupid <sunfeng32@qq.com>
2017-05-28 20:15:08 -0700

    feat(storybook-options):change name&url rightPanel

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,20 @@
 import { configure, setAddon } from '@storybook/react';
 import infoAddon from '@storybook/addon-info';
+import { setOptions } from '@storybook/addon-options';
+
+import pkg from '../starter/config/minimal-package';
 
 setAddon(infoAddon);
+setOptions({
+  name: pkg.name,
+  url: pkg.repository.url,
+  goFullScreen: false,
+  showLeftPanel: true,
+  showDownPanel: true,
+  showSearchBox: false,
+  downPanelInRight: true,
+  sortStoriesByKind: false,
+});
 
 function loadStories() {
   const req = require.context('../stories', true, /\.story\.js$/);
