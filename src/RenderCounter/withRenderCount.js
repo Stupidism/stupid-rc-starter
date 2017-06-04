2a3da09523e354d299d82f06878ed1a4a962a74d
stupid <sunfeng32@qq.com>
2017-06-04 15:36:25 -0700

    refactor(withRenderCount): mid handler onRerender

--- src/RenderCounter/withRenderCount.js
+++ src/RenderCounter/withRenderCount.js
@@ -3,17 +3,20 @@ import compose from 'recompose/compose';
 import withState from 'recompose/withState';
 import lifecycle from 'recompose/lifecycle';
 import flattenProp from 'recompose/flattenProp';
+import withHandlers from 'recompose/withHandlers';
 
 import copyStatics from '../hocs/copyStatics';
 import extendStatics from '../hocs/extendStatics';
 import omitPropTypes from '../hocs/omitPropTypes';
 import omitProps from '../hocs/omitProps';
 
-const componentWillUpdate = ({ state }) => {
-  state.count += 1; // eslint-disable-line no-param-reassign
-};
+const componentWillUpdate = ({ onRerender }) => onRerender();
 
-const getInitialState = ({ initialCount }) => ({ count: initialCount });
+const getInitialSelf = ({ initialCount }) => ({ count: initialCount });
+
+const onRerender = ({ self }) => () => {
+  self.count += 1;// eslint-disable-line no-param-reassign
+};
 
 export default Component => compose(
   omitPropTypes('count'),
@@ -23,8 +26,9 @@ export default Component => compose(
     defaultProps: { initialCount: 1 },
   }),
   copyStatics(Component),
-  withState('state', 'setState', getInitialState),
+  withState('self', 'setSelf', getInitialSelf),
+  withHandlers({ onRerender }),
   lifecycle({ componentWillUpdate }),
-  flattenProp('state'),
-  omitProps(['state', 'setState', 'initialCount']),
+  flattenProp('self'),
+  omitProps(['self', 'setSelf', 'onRerender', 'initialCount']),
 )(Component);
