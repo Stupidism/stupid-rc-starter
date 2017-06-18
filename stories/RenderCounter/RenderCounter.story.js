4b697a5746118684e07f2bc527710b7e349b7eb5
stupid <sunfeng32@qq.com>
2017-06-17 22:22:00 -0700

    docs(RenderCounter): remove DivRefreshable

--- stories/RenderCounter/RenderCounter.story.js
+++ stories/RenderCounter/RenderCounter.story.js
@@ -18,24 +18,6 @@ const stories = storiesOf('RenderCounter', module)
     return element;
   });
 
-stories.addWithInfo(
-  'RenderCounter',
-  `
-    This is the basic usage inside any component.\n
-    NOTICE: You may see them render some extra times. That's a bug of storybook when you enter this page directly.
-    Click another menu and re-enter this page to see real render times.
-  `,
-  () => <RenderCounter initialCount={number('initialCount', 0)} />,
-);
-
-const description = (
-  <div>
-    <h2>DivRefreshable</h2>
-    <p><strong>DivRefreshable</strong> is a div with a button that can refresh itself</p>
-    <p>It will update its only child if cloneChild prop is set to true</p>
-  </div>
-);
-
 const createUnstableHandler = (possibility = 0.5, log = action('onRerender')) => {
   const handler = (props, next) => (count) => {
     if (Math.random() < possibility) {
@@ -50,17 +32,26 @@ const createUnstableHandler = (possibility = 0.5, log = action('onRerender')) =>
   return handler;
 };
 
-stories.addWithInfo('inside DivRefreshable', description, () => {
-  const possibility = number('possibility', 0.5);
-  const onRerender = createUnstableHandler(possibility);
-  return (
-    <DivRefreshable
-      label={`Counter re-rendered in ${possibility * 100}% possibility`}
-    >
-      <RenderCounter onRerender={onRerender} />
-    </DivRefreshable>
-  );
-}, { propTablesExclude: [RenderCounter] });
+stories.addWithInfo(
+  'RenderCounter',
+  `
+    This is the basic usage inside any component.\n
+    NOTICE: You may see them render some extra times. That's a bug of storybook when you enter this page directly.
+    Click another menu and re-enter this page to see real render times.
+  `,
+  () => {
+    const possibility = number('possibility', 0.5);
+    const onRerender = createUnstableHandler(possibility);
+    return (
+      <DivRefreshable
+        label={`Counter re-rendered in ${possibility * 100}% possibility`}
+      >
+        <RenderCounter onRerender={onRerender} />
+      </DivRefreshable>
+    );
+  },
+  { propTablesExclude: [DivRefreshable] },
+);
 
 stories.addWithInfo('Counter', () => <Counter count={number('count', 1)} />);
 
