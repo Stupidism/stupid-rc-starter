a72ac7ed165dbc0aa1154df5b6e9ad39d8e42dfd
stupid <sunfeng32@qq.com>
2017-06-17 22:20:57 -0700

    feat(stories/DivRefreshable): replace btn, event

--- stories/RenderCounter/DivRefreshable.js
+++ stories/RenderCounter/DivRefreshable.js
@@ -1,30 +1,37 @@
 import React from 'react';
 import T from 'prop-types';
-import { compose, withState, withHandlers } from 'recompose';
+import { compose, withState, withHandlers, defaultProps } from 'recompose';
+import WithEvents from '@storybook/addon-events';
 
 import copyStatics from '../../src/hocs/copyStatics';
 import omitPropTypes from '../../src/hocs/omitPropTypes';
 
+const WithRefreshEvent = defaultProps({
+  events: [{
+    key: 'refresh',
+    name: 'REFRESH',
+    title: 'Refresh',
+  }],
+})(WithEvents);
+
 const DivRefreshable = ({ label, onRefresh, children }) => (
-  <div>
-    <button onClick={onRefresh}>{label}</button>
-    {children && React.cloneElement(children)}
-  </div>
+  <WithRefreshEvent emit={onRefresh}>
+    <div>
+      {label}
+      {children && React.cloneElement(children)}
+    </div>
+  </WithRefreshEvent>
 );
 
-const propTypes = {
+DivRefreshable.propTypes = {
   children: T.element,
   label: T.string,
   onRefresh: T.func.isRequired,
 };
-
-const defaultProps = {
+DivRefreshable.defaultProps = {
   children: undefined,
   label: 'Refresh',
 };
-
-DivRefreshable.propTypes = propTypes;
-DivRefreshable.defaultProps = defaultProps;
 
 const onRefresh = ({ setState }) => (/* event */) => setState();
 
