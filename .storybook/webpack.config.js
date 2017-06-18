ae524e17fcc1f192a5a1ec91f3578ba510c48d77
stupid <sunfeng32@qq.com>
2017-06-17 17:05:46 -0700

    chore(storybook): support css

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -7,6 +7,12 @@ module.exports = {
         test: /\.md$/,
         loaders: ['raw-loader'],
         include: path.resolve(__dirname, '../'),
+      }, {
+        test: /\.css$/,
+        use: [
+          { loader: "style-loader" },
+          { loader: "css-loader" }
+        ]
       },
     ],
   },
