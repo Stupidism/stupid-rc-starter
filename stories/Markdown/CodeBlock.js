c48262f7f66b8ad04b9219cce650c4f3f1e48337
stupid <sunfeng32@qq.com>
2017-06-24 15:50:16 -0700

    fix(stories/Markdown/CodeBlock):pick usefull props

--- stories/Markdown/CodeBlock.js
+++ stories/Markdown/CodeBlock.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import T from 'prop-types';
+import _ from 'lodash';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { atelierCaveLight } from 'react-syntax-highlighter/dist/styles';
 
 const CodeBlock = ({ literal, ...rest }) => (
-  <SyntaxHighlighter {...rest}>{literal}</SyntaxHighlighter>
+  <SyntaxHighlighter {..._.pick(rest, ['language', 'style'])}>{literal}</SyntaxHighlighter>
 );
 
 CodeBlock.propTypes = {
