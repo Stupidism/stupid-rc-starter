633f490438b0f01794657233e450cdc65f518f01
stupid <sunfeng32@qq.com>
2018-02-28 17:08:56 +0800

    chore(package): upgrade breaking depens

--- stories/Markdown/CodeBlock.js
+++ stories/Markdown/CodeBlock.js
@@ -2,7 +2,7 @@ import React from 'react';
 import T from 'prop-types';
 import _ from 'lodash';
 import SyntaxHighlighter from 'react-syntax-highlighter';
-import { atelierCaveLight } from 'react-syntax-highlighter/dist/styles';
+import atelierCaveLight from 'react-syntax-highlighter/dist/styles/hljs/atelier-cave-light';
 
 const CodeBlock = ({ literal, ...rest }) => (
   <SyntaxHighlighter {..._.pick(rest, ['language', 'style'])}>{literal}</SyntaxHighlighter>
